24e54a96e681d080031a96968d50fd9b5607ef6f
LucSilveira <lucas1-portal@hotmail.com>
2020-01-08 11:28:15 -0300

    Subindo correções no front da aplicação

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,9 +1,10 @@
 import React, {useState, useMemo} from 'react';
 import camera from '../../assets/camera.svg';
 import './styles.css';
-import { url } from 'inspector';
+import api from '../../services/api' 
+// import { url } from 'inspector';
 
-export default function New(){
+export default function New({ history }){
     const [thumbnail, setThumbnail] = useState(null);
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
@@ -13,13 +14,29 @@ export default function New(){
         return thumbnail ? URL.createObjectURL(thumbnail) : null
     }, [thumbnail])
 
-    function handleSubmit(){
+    async function handleSubmit(event){
+        event.preventDefault()
+        const user_id = localStorage.getItem('user')
 
+        const data = new FormData();
+        data.append('thumbnail', thumbnail)
+        data.append('company', company);
+        data.append('techs', techs);
+        data.append('price', price)
+
+        await api.post('/spots', data, {
+            headers : {
+                user_id
+            }
+        })
+
+        history.push('/dashboard');
     }
-
+   
     return (
         <form onSubmit={handleSubmit}>
-            <label id="thumbnail" style={{backgroundImage: `url(${preview})`}}>
+            <label id="thumbnail" style={{backgroundImage: `url(${preview})`}}
+                className={thumbnail ? 'has-thumbnail' : ''}>
                 <input type="file" onChange={event => setThumbnail(event.target.files[0])}/>
                 <img src={camera} alt="Selecione a imagem"/>
             </label>
@@ -31,7 +48,7 @@ export default function New(){
                 value={company}
                 onChange={event => setCompany(event.target.value)}
             />
-            <label htmlFor="techs">Tecnologias * <span>(em branco para gratuito)</span></label>
+            <label htmlFor="techs">Tecnologias * <span>(separadas por vírgulas)</span></label>
             <input 
                 type="text"
                 id="techs"
@@ -39,7 +56,7 @@ export default function New(){
                 value={techs}
                 onChange={event => setTechs(event.target.value)}
             />
-            <label htmlFor="price">Valor da diaria * <span>(separadas por vírgulas)</span></label>
+            <label htmlFor="price">Valor da diaria * <span>(em branco para gratuito)</span></label>
             <input 
                 type="text"
                 id="price"
